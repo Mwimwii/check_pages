cfae30d52bbb803f06f053a26f5bcdc658f9085f
Paul M. S <29229275+ItIsEntropy@users.noreply.github.com>
2021-03-25 10:59:52 +0200

    Added dict config. waiting for MSTF webhook

--- check_pages.py
+++ check_pages.py
@@ -3,6 +3,11 @@ import json
 import time
 import os
 import datetime
+import requests
+import logging
+import logging.config
+import urllib
+from requests import status_codes
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import StaleElementReferenceException
@@ -14,9 +19,45 @@ from selenium.webdriver.common.keys import Keys
 from typing import List, Set
 from pathlib import Path
 from urllib.parse import urlparse
-import logging as log
+from requests.exceptions import MissingSchema, InvalidSchema, InvalidURL
+from teams_logger import TeamsQueueHandler, Office365CardFormatter
 
-
+# Configure the logger to send messages to Teams
+# declare useful variables
+# webhook url
+teams_webhook_url: str = ''
+# dict to configger the logger
+config = {
+    'version' : 1,
+    'disable_existing_loggers' : False,
+    'Formatters' : {
+        'teamscard' : {
+            '()' : Office365CardFormatter,
+            'facts' : ['name', 'levelname', 'lineno'],
+        },
+    },
+    'handlers' : {
+        'msteams' : {
+            'level' : logging.INFO,
+            'class' : 'teams_logger.TeamsQueueHandler',
+            'url' : teams_webhook_url,
+            'formatter' : 'teamscard',
+        },
+    },
+    'loggers' : {
+        __name__ : {
+            'handlers' : ['msteams'],
+            'level' : logging.DEBUG,
+        }
+    },
+}
+logging.config.dictConfig(config)
+log = logging.getLogger(__name__)
+log.info('Info level test')
+log.debug('debug level prompt')
+log.warning('Things are serious, warning level test')
+log.error('yikes, error level message. maybe do something about it now?')
+log.critical("welp,it's too late now, critical failure message")
 
 # Variables:
 #           - Offline Sites list that will store offline sites.
@@ -26,6 +67,8 @@ offline_sites: list = []
 sites: list = ["http://www.sh.gov.zm/", "http://www.ovp.gov.zm/", "http://www.cabinet.gov.zm/", "http://www.psmd.gov.zm/", "http://www.ago.gov.zm/", "http://www.mod.gov.zm/", "http://www.moha.gov.zm/", "http://www.mofa.gov.zm/", "http://www.mof.gov.zm/", "http://www.moj.gov.zm/", "http://www.mndp.gov.zm/", "http://www.agriculture.gov.zm/", "http://www.mfl.gov.zm/", "http://www.mcti.gov.zm/", "http://www.mmmd.gov.zm/", "http://www.moe.gov.zm/", "http://www.mota.gov.zm/", "http://www.mtc.gov.zm/", "http://www.mws.gov.zm/", "http://www.mhid.gov.zm/", "http://www.mlnr.gov.zm/",
          "http://www.mwdsep.gov.zm/", "http://www.moh.gov.zm/", "http://www.mibs.gov.zm/", "http://www.moge.gov.zm/", "http://www.mohe.gov.zm/", "http://www.mlgh.gov.zm/", "http://www.mlss.gov.zm/", "http://www.mcdsw.gov.zm/", "http://www.gender.gov.zm/", "http://www.myscd.gov.zm/", "http://www.mocta.gov.zm/", "http://www.mngra.gov.zm/", "http://www.cen.gov.zm/", "http://www.cbt.gov.zm/", "http://www.eas.gov.zm/", "http://www.lua.gov.zm/", "http://www.lsk.gov.zm/", "http://www.muc.gov.zm/", "http://www.nws.gov.zm/", "http://www.nor.gov.zm/", "http://www.sou.gov.zm/", "http://www.wes.gov.zm/"]
 visited_pages: Set[str] = set()
+broken_links: dict = {}
+hostname: str = ''
 
 def init():
 
@@ -98,7 +141,8 @@ def check_name(file_name: Path, copy: int = 2) -> str:
         file_name = Path(f'{str(file_name)[:-4]}({copy}).PNG')
         if file_name.exists():
             # get the number in the braces, turn it into an int, increment by 1, recurse
-            copy = int(str(file_name).split('(')[1][:-5]) + 1
+            copy = 2# int(str(file_name).split('(')[1][:-5]) + 1
+            #copy = int(re.match)
             file_name = Path(f'{str(file_name)[:-4]}({copy}).PNG')
             return check_name(file_name=file_name, copy=copy)
     
@@ -106,28 +150,55 @@ def check_name(file_name: Path, copy: int = 2) -> str:
 
 def get_links(path: Path, driver: webdriver.Chrome):
     ignored_exceptions = (NoSuchElementException, StaleElementReferenceException,)
-    # get all links in the page by getting anchor tags then their hrefs
-    links: List[str] = [x.get_attribute('href') for x in driver.find_elements_by_tag_name("a")] 
+    # get all links in the page by getting anchor tags then their hrefs and text, skiping links to external origins.
+    anchors = driver.find_elements_by_tag_name('a')
+    links: List[tuple] = [(x.get_attribute('href'), x.text) for x in anchors if x.get_attribute('href').startswith(hostname)]#if x.text] 
+    print(f'\n\nLinks:\n\t{links}\n\n')
     # traverse set of anchor tags
     
     for ref in links:
-        if ref is not None:
+        if ref[0] is not None:
             # if the link has been visited before, skip to next link (interation)
-            if ref in visited_pages:
+            if ref[0] not in visited_pages:
                 continue
-            if ref.endswith('#'):
-                pass 
-            name = ref.split('/')[-1]
-            photo_name: Path = path.joinpath(f'{name}.png')
-            save_screenshot(driver=driver, file_name=photo_name, url= ref, include_scrollbar= False)
-            # Recursively traverse the site for all anchor tags leading to a link
+            # skip empty anchor tags
+            if ref[0].endswith('#'):
+                continue
+            try:
+                request = requests.head(ref[0], data= {'key', 'value'})
+                # Report all broken links
+                if request.status_code < 200 or request.status_code > 299:
+                    broken_links[ref[0]] = {'status': request.status_code, 'reason': request.reason, 'text': str(request.status_code),}
+                # Report urls with empty link text and skip them
+                if not ref[1]:
+                    broken_links[ref[0]] = {'status': request.status_code, \
+                        'reason': 'Empty anchor tag', \
+                            'text': 'Link skipped because it does not have a human readable name.',}
+                    log.error(f'Site ')
+                    continue
+            except MissingSchema:
+                broken_links[ref[0]] = {'status': request.status_code, \
+                        'reason': 'Mising Schema Exception', \
+                            'text': 'Missing Schema Exception from requests',}
+            except InvalidSchema:
+                broken_links[ref[0]] = {'status': request.status_code, \
+                        'reason': 'Invalid Schema Exception', \
+                            'text': 'Invalid Schema Exception from requests',}
+            except Exception as e:
+                log.error(f'Exception: {e}', exc_info = True)
+                pass
+            name = ref[0].split('/')[-1]
+            # photo_name: Path = path.joinpath(f'{name}.png')
+            photo_name: Path = path.joinpath(f'link name: {ref[1]}.png')
+            save_screenshot(driver=driver, file_name=photo_name, url= ref[0], include_scrollbar= False)
+            # Recursivel  y traverse the site for all anchor tags leading to a link
             #get_links(path=path, driver=driver)
     
              
 
 def check_site(url: str, path: Path, driver: webdriver.Chrome):
-    host_name: str = urlparse(url).hostname
-    dir: Path = make_dir(parent=path, name=host_name)
+    hostname = urlparse(url).hostname
+    dir: Path = make_dir(parent=path, name=hostname)
     home_pic = dir.joinpath('home.png')
     save_screenshot(driver=driver, file_name= home_pic, url=url, include_scrollbar=False)
     get_links(path=dir, driver=driver)
@@ -140,7 +211,6 @@ def check_sites(driver: webdriver.Chrome, sites: List[str], path: Path):
     for site in sites:
         try:
             print(f"Getting site: {site}")
-            file_name: Path = Path(f"{path}\\{urlparse(site).hostname}.png")
             # driver.save_screenshot(path)
             # Save a snapshot of the site page inside todays folder
             check_site(url=site, path=path, driver = driver)
@@ -152,8 +222,13 @@ def check_sites(driver: webdriver.Chrome, sites: List[str], path: Path):
             raise e
     driver.close()
     # Save the offline sites in a json file
-    with open('offline_sites.json', 'w+') as f:
-        f.write(json.dumps(offline_sites))
+    if offline_sites:
+        with open('offline_sites.json', 'w+') as f:
+            f.write(json.dumps(offline_sites))
+    # Save the broken links sites in a json file
+    if broken_links:
+        with open('broken_links.json', 'w+') as f:
+            f.write(json.dumps(broken_links))
 
 if __name__ == '__main__':
     init()
